9ab2cdf2f207fd1ffc404e06f10b8e63d8164507
Ana Ceaicovski <ceaicovski94@gmail.com>
2024-03-10 00:31:38 +0200

    finished UI part

--- App.js
+++ App.js
@@ -1,20 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
+import dayjs from "dayjs";
+import wallpaper from "./assets/images/wallpaper.webp";
+import NotificationsList from "./components/NotificationList";
 
 export default function App() {
+  const [date, setDate] = useState(dayjs());
+
+  useEffect(() => {
+    let timer = setInterval(() => {
+      setDate(dayjs());
+    }, 1000 * 1);
+
+    return () => clearInterval(timer);
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground source={wallpaper} style={styles.container}>
+      <NotificationsList
+        ListHeaderComponent={() => (
+          <View style={styles.header}>
+            <Ionicons name="lock-closed" size={24} color="white" />
+            <Text style={styles.date}>{date.format("dddd, DD MMMM")}</Text>
+            <Text style={styles.time}>{date.format("hh:mm")}</Text>
+          </View>
+        )}
+      />
+
+      <View style={styles.footer}>
+        <View style={styles.icon}>
+          <MaterialCommunityIcons name="flashlight" size={24} color="white" />
+        </View>
+
+        <View style={styles.icon}>
+          <Ionicons name="camera" size={24} color="white" />
+        </View>
+      </View>
+
+      <StatusBar style="light" />
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "stretch",
+  },
+  header: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: 300,
+  },
+  date: {
+    color: "#C3FFFE",
+    fontSize: 20,
+    fontWeight: "500",
+    marginTop: 20,
+  },
+  time: {
+    fontSize: 82,
+    fontWeight: "bold",
+    color: "#C3FFFE",
+  },
+  footer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: "auto",
+    paddingHorizontal: 30,
+    paddingVertical: 10,
+    alignSelf: "stretch",
+    height: 75,
+  },
+  icon: {
+    backgroundColor: "#00000050",
+    width: 50,
+    aspectRatio: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 100,
+    overflow: "hidden",
   },
 });
+
